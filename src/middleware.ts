6b11930317bcc5f3a5e52f801012783a0408cac3
Stojan Petkovic <petkovicstojan@gmail.com>
2026-07-12 21:19:18 +0200

    Opis izmene

---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+// src/middleware.ts
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  return next(); // Samo propusti sve
+});
